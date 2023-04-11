a43faa747d45b8061d05fad48ec8ec857c39579a
John Cobb <jdcobb3@gmail.com>
2023-04-11 18:23:16 -0500

    classical gcn models

---
+++ models/baselines/pa_gnn.py
@@ -0,0 +1,25 @@
+"""
+Copyright 2020 Twitter, Inc.
+SPDX-License-Identifier: Apache-2.0
+"""
+import torch
+
+from utilities.laplacian import normalized_graph_laplacian
+
+
+class PaGNNConv(torch.nn.Module):
+    def __init__(self, in_features, out_features, mask, edge_index):
+        super(PaGNNConv, self).__init__()
+        self.lin = torch.nn.Linear(in_features, out_features)
+        self.mask = mask.float()
+        edge_index, edge_weight = normalized_graph_laplacian(edge_index, mask.shape[0])
+        self.adj = torch.sparse.FloatTensor(edge_index, values=edge_weight).to(edge_index.device)
+
+    def forward(self, x, edge_index):
+        x[x.isnan()] = 0
+        numerator = torch.sparse.mm(self.adj, torch.ones_like(x)) * torch.sparse.mm(self.adj, self.mask * x)
+        denominator = torch.sparse.mm(self.adj, self.mask)
+        ratio = torch.nan_to_num(numerator / denominator)
+        x = self.lin(ratio)
+
+        return x
