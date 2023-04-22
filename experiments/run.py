fe5be807110e3426904e0cc1b0f931c4de7384ab
John Cobb <jdcobb3@gmail.com>
2023-04-22 17:30:27 -0500

    Update run.py

--- experiments/run.py
+++ experiments/run.py
@@ -128,7 +128,7 @@ def run(args):
                 rate=args.missing_rate, n_nodes=n_nodes, n_features=n_features, type=args.mask_type,
             ).to(device)
             x = data.x.clone()
-           # x[~missing_feature_mask] = float("nan") This makes absolutely sure that I don't use the hidden of values of x to extend! However, we make sure to apply the mask at every opportunity OTHER than to compute the laplacian. I should implement a way to make sure that works with missing values also.....
+            # x[~missing_feature_mask] = float("nan") This makes absolutely sure that I don't use the hidden of values of x to extend! However, we make sure to apply the mask at every opportunity OTHER than to compute the laplacian. I should implement a way to make sure that works with missing values also.....
             y = data.y.clone()
 
             logger.debug("Starting feature filling")
